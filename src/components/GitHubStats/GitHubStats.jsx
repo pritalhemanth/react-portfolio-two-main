e9df62ca4b5f613a524cc50c0a3b641135244181
Prital <pritalhemanth@gmail.com>
2026-09-24 23:26:30 +0530

    last_change

--- src/components/GitHubStats/GitHubStats.jsx
+++ src/components/GitHubStats/GitHubStats.jsx
@@ -14,7 +14,7 @@ function GitHubStats({ username }) {
             try {
                 setLoading(true);
                 // Talk to the GitHub API Waiter
-                const response = await fetch(`https://api.github.com/users/${pritalhemanth}`);
+                const response = await fetch(`https://api.github.com/users/${username}`);
                 
                 if (!response.ok) throw new Error('User not found');
                 
